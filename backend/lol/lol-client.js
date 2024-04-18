a6d4b864e8201b6ce441fcce35992f7fee3814e0
Philip Ng Pack <40671598+philipngpack@users.noreply.github.com>
2024-04-17 22:33:34 -0400

    Backend riot (#2)
    * add riot api to backend

--- backend/lol/lol-client.js
+++
@@ -1,8 +0,0 @@
-import { LolApi, Constants } from 'twisted'
-
-const api = new LolApi()
-
-export async function getSummoner () {
-  const user = await getAccount()
-  return await api.Summoner.getByPUUID(user.puuid, Constants.Regions.AMERICA_NORTH)
-}
--- backend/lol/riot-client.js
+++
@@ -1,8 +0,0 @@
-import { RiotApi, Constants } from 'twisted'
-
-const api = new RiotApi()
-
-export async function getAccount () {
-  // Recommended to use the nearest routing value to your server: americas, asia, europe
-  return (await api.Account.getByRiotId("Hide on bush", "KR1", Constants.RegionGroups.AMERICAS)).response
-}
