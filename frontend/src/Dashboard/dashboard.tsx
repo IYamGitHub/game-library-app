1bf55d9731f422f42188090cd6a5df703d802ade
Lucy Ying <lucyying@Lucys-MacBook-Pro.local>
2024-04-22 22:39:42 -0400

    prettifying the dashboard and fixing up the carousel

--- frontend/src/Dashboard/dashboard.tsx
+++ frontend/src/Dashboard/dashboard.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import Carousel from './carousel';
+import "./dashboard.css";
+
 const Dashboard = () => {
   return (
     <div className="w-100">
-      <h1>Dashboard</h1>
+      <div className='dashboard-header'>
+        <h1>Dashboard</h1>
+      </div>
+      <h3>Most Liked Games on Flammie!</h3>
       <Carousel />
     </div>
   );
