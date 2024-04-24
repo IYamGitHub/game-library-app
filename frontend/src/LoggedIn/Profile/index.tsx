2c89d52cf7f63b2d2ed781106e8f3fe2a5aaa24a
philipngpack <ngpack.p@northeastern.edu>
2024-04-24 16:32:48 -0400

    make hr below games title

--- frontend/src/LoggedIn/Profile/index.tsx
+++ frontend/src/LoggedIn/Profile/index.tsx
@@ -159,7 +159,7 @@ const Profile = ({ onRefresh }: ProfileProps) => {
         setShowModal={setShowModal}
         submitAvatar={submitAvatar}
       />
-      <div>
+      <div style={{ marginBottom: '40px' }}>
         <div className="profile-header d-flex justify-content-between">
           <div className="d-flex align-items-center">
             <div
@@ -216,7 +216,7 @@ const Profile = ({ onRefresh }: ProfileProps) => {
                 Follow
               </button>
               {adminUser && 
-              (<button className="btn btn-danger px-4 me-2" onClick={deleteUser}>
+              (<button className="btn btn-danger px-4" onClick={deleteUser}>
                 Delete
               </button>)}
             </div>
@@ -263,8 +263,8 @@ const Profile = ({ onRefresh }: ProfileProps) => {
           )}
         </div>
       </div>
-      <hr/>
       <h1>Games</h1>
+      <hr/>
       <CardRow />
     </>
   );
